b9158bbc31c87f7fb3c288d4699c87afa5392b1b
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2019-03-04 12:52:32 -0500

    Refactor

--- routes/comments.js
+++ routes/comments.js
@@ -2,12 +2,9 @@
 /* eslint-disable consistent-return */
 /* eslint-disable no-underscore-dangle */
 const express = require('express'),
-  Room = require('../models/room.js'),
   Comment = require('../models/comment.js'),
   middleware = require('../middleware'),
   router = express.Router({ mergeParams: true });
-
-const noRoomFound = 'No room found with that ID.';
 
 // Comments - New
 router.get('/new', middleware.checkRoom, (req, res) => {
@@ -15,58 +12,41 @@ router.get('/new', middleware.checkRoom, (req, res) => {
 });
 
 // Comments - Create
-router.post('/', middleware.isLoggedIn, async (req, res) => {
+router.post('/', middleware.checkRoom, middleware.isLoggedIn, async (req, res) => {
   // lookup room using ID
-  try {
-    const room = await Room.findById(req.params.id),
-      comment = await Comment.create(req.body.comment);
-    // add username and id to comment
-    comment.author.id = req.user._id;
-    comment.author.username = req.user.username;
-    // save comment
-    comment.save();
-    room.comments.push(comment);
-    room.save();
-    req.flash('success', 'Successfully added comment');
-    res.redirect(`/rooms/${room._id}`);
-  } catch (e) {
-    req.flash('error', noRoomFound);
-    res.redirect('/rooms');
-  }
+  const comment = await Comment.create(req.body.comment);
+  // add username and id to comment
+  comment.author.id = req.user._id;
+  comment.author.username = req.user.username;
+  comment.save();
+  req.room.comments.push(comment);
+  req.room.save();
+  req.flash('success', 'Successfully added comment');
+  res.redirect(`/rooms/${req.room._id}`);
 });
 
 // Comments - Edit
-router.get('/:comment_id/edit', middleware.checkCommentOwnership, async (req, res) => {
-  try {
-    const room = await Room.findById(req.params.id);
-    if (!room) throw new Error(noRoomFound);
-    const comment = await Comment.findById(req.params.comment_id);
-    res.render('comments/edit', { room_id: req.params.id, comment });
-  } catch (e) {
-    req.flash('error', noRoomFound);
-    res.redirect('/rooms');
-  }
+router.get('/:comment_id/edit', middleware.checkRoom, middleware.checkCommentOwnership, async (req, res) => {
+  const comment = await Comment.findById(req.params.comment_id);
+  res.render('comments/edit', { room_id: req.params.id, comment });
 });
 
 // Comments - Update
-router.put('/:comment_id', middleware.checkCommentOwnership, (req, res) => {
-  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, err => {
-    if (err) res.redirect('back');
-    else res.redirect(`/rooms/${req.params.id}`);
-  });
+router.put('/:comment_id', middleware.checkCommentOwnership, async (req, res) => {
+  await Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment);
+  res.redirect(`/rooms/${req.params.id}`);
 });
 
 // Comments - Delete
-router.delete('/:comment_id', middleware.checkCommentOwnership, (req, res) => {
-  Comment.findByIdAndDelete(req.params.comment_id, err => {
-    if (err) {
-      req.flash('error', 'Something went wrong');
-      res.redirect('back');
-    } else {
-      req.flash('success', 'Comment deleted');
-      res.redirect(`/rooms/${req.params.id}`);
-    }
-  });
+router.delete('/:comment_id', middleware.checkCommentOwnership, async (req, res) => {
+  try {
+    await Comment.findByIdAndDelete(req.params.comment_id);
+    req.flash('success', 'Comment deleted');
+    res.redirect(`/rooms/${req.params.id}`);
+  } catch (e) {
+    req.flash('error', 'Something went wrong deleting comment');
+    res.redirect('back');
+  }
 });
 
 module.exports = router;
