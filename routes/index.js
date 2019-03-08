eb15405404cb3511a4ce2c1c5a75ef4d2ba8c531
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2019-03-08 18:35:24 -0500

    Refactor user auth pages
    Fix register success flash
    Add user auth page partials

--- routes/index.js
+++ routes/index.js
@@ -19,10 +19,9 @@ router.post('/register', (req, res) => {
   const newUser = new User({ username: req.body.username });
   User.register(newUser, req.body.password, (err, user) => {
     if (err) return res.render('register', { error: err.message });
-    passport.authenticate('local')(req, res, () => {
-      req.flash('success', `Successfully Signed Up! Nice to meet you ${user.username}`);
-    });
-    return res.redirect('/rooms');
+    passport.authenticate('local');
+    req.flash('success', `Successfully signed Up ${user.username}!`);
+    res.redirect('/rooms');
   });
 });
 
@@ -39,14 +38,13 @@ router.post(
     failureRedirect: '/login',
     failureFlash: true,
     successFlash: 'Welcome back to RoomGenie!'
-  }),
-  (req, res) => {}
+  })
 );
 
 // logout route
 router.get('/logout', (req, res) => {
   req.logout();
-  req.flash('success', 'Logged you out!');
+  req.flash('success', 'Successfully logged you out.');
   res.redirect('/rooms');
 });
 
