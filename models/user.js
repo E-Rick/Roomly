0541421f1d6865caa6355150076c9a354f75909d
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2019-03-12 19:49:24 -0400

    Add user profiles

--- models/user.js
+++ models/user.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose'),
   passportLocalMongoose = require('passport-local-mongoose');
 
-const UserSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  isAdmin: { type: Boolean, default: false }
-});
+const UserSchema = new mongoose.Schema(
+  {
+    username: String,
+    password: String,
+    avatar: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    isAdmin: { type: Boolean, default: false }
+  },
+  {
+    // if timestamps are set to true, mongoose assigns createdAt and
+    // updatedAt fields to your schema, the type assigned is Date.
+    timestamps: { createdAt: true, updatedAt: false }
+  }
+);
 
 UserSchema.plugin(passportLocalMongoose);
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 const express = require('express'),
   passport = require('passport'),
   User = require('../models/user'),
+  Room = require('../models/room'),
   router = express.Router();
 
 // Root route
@@ -16,7 +17,13 @@ router.get('/register', (req, res) => {
 
 // Handle signup logic route
 router.post('/register', (req, res) => {
-  const newUser = new User({ username: req.body.username });
+  const newUser = new User({
+    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    avatar: req.body.avatar
+  });
   User.register(newUser, req.body.password, (err, user) => {
     if (err) return res.render('register', { error: err.message });
     passport.authenticate('local');
@@ -46,6 +53,26 @@ router.get('/logout', (req, res) => {
   req.logout();
   req.flash('success', 'Successfully logged you out.');
   res.redirect('/rooms');
+});
+
+// User profiles
+router.get('/users/:id', (req, res) => {
+  User.findById(req.params.id, (err, foundUser) => {
+    if (err) {
+      req.flash('error', 'user not found');
+      res.redirect('/rooms');
+    }
+    Room.find()
+      .where('author.id')
+      .equals(foundUser._id)
+      .exec((error, rooms) => {
+        if (error) {
+          req.flash('error', 'user not found');
+          res.redirect('/rooms');
+        }
+        res.render('users/show', { user: foundUser, rooms });
+      });
+  });
 });
 
 module.exports = router;
