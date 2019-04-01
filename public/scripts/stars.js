7baffac43cb4e9f3bcdb6d5fcca95f64cfa08985
E-rick <erickmartinezjunior@gmail.com>
2019-04-01 14:35:59 -0400

    Change hamburger to arrow

--- public/scripts/stars.js
+++ public/scripts/stars.js
@@ -2,19 +2,18 @@
 /* eslint-disable no-unused-vars */
 var count;
 function starmark(item) {
-  var i = 0;
-  count = item.id[0];
-  sessionStorage.starRating = count;
-  const subid = item.id.substring(1);
-  for (i = 0; i < 5; i++) {
-    if (i < count) {
-      document.getElementById(i + 1 + subid).style.color = 'orange';
-    } else {
-      document.getElementById(i + 1 + subid).style.color = 'black';
-    }
-  }
+	var i = 0;
+	count = item.id[0];
+	sessionStorage.starRating = count;
+	const subid = item.id.substring(1);
+	for (i = 0; i < 5; i++) {
+		if (i < count) {
+			document.getElementById(i + 1 + subid).style.color = 'orange';
+		} else {
+			document.getElementById(i + 1 + subid).style.color = 'black';
+		}
+	}
 }
-
 function result() {
-  if (count) document.getElementById('rating').value = count;
+	if (count) document.getElementById('rating').value = count;
 }
