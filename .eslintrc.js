47b090c50a127ca7c922e63611c170d9fea46ad2
E-rick <erickmartinezjunior@gmail.com>
2019-04-15 14:59:03 -0400

    eslint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,46 @@
 module.exports = {
-  extends: 'airbnb-base',
-  env: {
-    browser: true,
-    node: true
-  },
-  rules: {
-    'linebreak-style': 0, // <----------
-    'prefer-const': [
-      'error',
-      {
-        destructuring: 'any',
-        ignoreReadBeforeAssign: false
-      }
-    ],
-    'comma-dangle': ['error', 'never'],
-    'one-var': ['error', 'consecutive'],
-    'no-underscore-dangle': ['error', { allow: ['_id'] }],
-    'arrow-parens': ['error', 'as-needed'],
-    'max-len': ['error', { code: 125 }],
-    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    'object-curly-newline': [
-      'error',
-      {
-        ImportDeclaration: 'never',
-        ExportDeclaration: { multiline: true, minProperties: 3 }
-      }
-    ]
-  }
+	extends : 'airbnb-base',
+	env     : {
+		browser : true,
+		node    : true
+	},
+	rules   : {
+		'linebreak-style'      : 0, // <----------
+		'prefer-const'         : [
+			'error',
+			{
+				destructuring          : 'any',
+				ignoreReadBeforeAssign : false
+			}
+		],
+		'comma-dangle'         : [ 'error', 'never' ],
+		indent                 : [ 2, 'tab' ],
+		'key-spacing'          : [
+			2,
+			{
+				singleLine : {
+					beforeColon : false,
+					afterColon  : true
+				},
+				multiLine  : {
+					beforeColon : true,
+					afterColon  : true,
+					align       : 'colon'
+				}
+			}
+		],
+		'no-tabs'              : [ 'error', { allowIndentationTabs: true } ],
+		'one-var'              : [ 'error', 'consecutive' ],
+		'no-underscore-dangle' : [ 'error', { allow: [ '_id' ] } ],
+		'arrow-parens'         : [ 'error', 'as-needed' ],
+		'max-len'              : [ 'error', { code: 125 } ],
+		'no-plusplus'          : [ 'error', { allowForLoopAfterthoughts: true } ],
+		'object-curly-newline' : [
+			'error',
+			{
+				ImportDeclaration : 'never',
+				ExportDeclaration : { multiline: true, minProperties: 3 }
+			}
+		]
+	}
 };
